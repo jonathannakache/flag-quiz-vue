e7d6f578720c8f540eb726f3827cde1944aad8e0
Jonathan Nakache <john.nakache@gmail.com>
2020-10-01 15:29:50 +0300

    init + creation du quiz

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>
+      TEST : Saurez-vous reconnaître ces pays par leur drapeau ?
+    </h1>
+    <card v-for="(item, index) in flags" :key="index" :flag="item"></card>
+
+    <div class="card">
+      <img
+        :src="`https://flagcdn.com/w640/${flags[index].flag}.png`"
+        class="card-img-top"
+        :alt="`Drapeau du ${flags[index].country} `"
+      />
+      <ul class="list-group">
+        <li
+          v-for="(item, index) in flags[index].answers"
+          :key="index"
+          class="list-group-item"
+        >
+          {{ item }}
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import flags from "./flags";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  methods: {},
+  data() {
+    return {
+      flags: flags,
+      index: 3,
+    };
+  },
+};
 </script>
 
 <style>
@@ -24,5 +47,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.card {
+  width: 50%;
+  margin: 0 auto;
+}
+.list-group-item {
+  cursor: pointer;
+  font-size: 18px;
+}
+.list-group-item:hover {
+  background: rgb(148, 205, 230);
 }
 </style>
---
+++ src/flags.js
@@ -0,0 +1,60 @@
+export default [
+  {
+    country: "Suisse",
+    flag: "ch",
+    answers: ["Suisse", "Roumanie", "Thailande", "Allemagne"],
+  },
+  {
+    country: "Tonga",
+    flag: "to",
+    answers: ["Tonga", "Angleterre", "Suede", "Suisse"],
+  },
+
+  {
+    country: "France",
+    flag: "fr",
+    answers: ["Angleterre", "Suisse", "France", "Andorre"],
+  },
+
+  {
+    country: "Nouvelle-Zelande",
+    flag: "nz",
+    answers: ["Angleterre", "Nouvelle-Zelande", "Australie", "Royaume-Uni"],
+  },
+
+  {
+    country: "Senegal",
+    flag: "sn",
+    answers: ["Mali", "Guinée", "Cameroune", "Sénégal"],
+  },
+
+  {
+    country: "Malaisie",
+    flag: "my",
+    answers: ["Europe", "Liberia", "Ouzbekistan", "Malaisie"],
+  },
+
+  {
+    country: "Viêt Nam",
+    flag: "vn",
+    answers: ["Maroc", "Corée du Nord", "Viêt Nam", "Chine"],
+  },
+
+  {
+    country: "Egypte",
+    flag: "eg",
+    answers: ["Libye", "Soudan", "Italie", "Egypte"],
+  },
+
+  {
+    country: "Djibouti",
+    flag: "dj",
+    answers: ["Les comores", "Djibouti", "Yémen", "Libye"],
+  },
+
+  {
+    country: "Colombie",
+    flag: "co",
+    answers: ["L'inde", `L'equateur`, "Le Venezuela", "La Combie"],
+  },
+];
